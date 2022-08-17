f051a9ececad4c58f26a173e09832a76f7e4765e
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-08-17 19:58:49 -0300

    Add files via upload

---
+++ JWTToken/Program.cs
@@ -0,0 +1,56 @@
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Builder;
+using JWT.Exceptions;
+using JWT.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace JWTToken
+{
+    class Program
+    {
+        private static RSA certificate;
+
+        static void Main(string[] args)
+        {
+            const string secret = "yourSecret";
+
+            var token = JwtBuilder.Create()
+                .WithAlgorithm(new HMACSHA256Algorithm())
+                .WithSecret(secret)
+                .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
+                .AddClaim("Primeiro", 1)
+                .AddClaim("Segundo", "Mxtheuz")
+                .Encode();
+
+            Console.WriteLine(token);
+
+            try
+            {
+                IJsonSerializer serializer = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtAlgorithm algorithm = new HMACSHA256Algorithm();
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+
+                var payload = decoder.DecodeToObject<IDictionary<string, object>>(token);
+                Console.WriteLine(payload["Segundo"]);
+            }
+            catch (TokenNotYetValidException)
+            {
+                Console.WriteLine("Token is not valid yet");
+            }
+            catch (TokenExpiredException)
+            {
+                Console.WriteLine("Token has expired");
+            }
+            catch (SignatureVerificationException)
+            {
+                Console.WriteLine("Token has invalid signature");
+            }
+        }
+    }
+}
